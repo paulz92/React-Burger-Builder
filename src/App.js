d0b63d023a159787cdaf2121f02d1cfe543989ac
Paul Zavattieri <paulz92@gmail.com>
2018-03-05 12:08:44 -0500

    my order db retrieval

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <Layout>
           <Switch>
             <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
             <Route path="/" component={BurgerBuilder} />
           </Switch>
         </Layout>
